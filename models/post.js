4f16f6d4c7929857ac3dcaff667cd1b214e11941
KanekarSnehal <kanekarsnehal43@gmail.com>
2024-01-02 15:27:38 +0530

    feat: add post model

---
+++ models/post.js
@@ -0,0 +1,37 @@
+const sequelize = require('../connections/mysql');
+const { DataTypes } = require('sequelize');
+
+const Post = sequelize.define('Post', {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    description: DataTypes.STRING,
+    created_by: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        references: {
+            model: 'User',
+            key: 'id',
+            allowNull: false
+        }
+    },
+    like_count: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        defaultValue: 0,
+    },
+    comment_count: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        defaultValue: 0,
+    },
+    created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+    },
+    updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+    }
+});
