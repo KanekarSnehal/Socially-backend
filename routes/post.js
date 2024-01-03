1d092d86f6076504520ce95f250fb1547db64f79
KanekarSnehal <kanekarsnehal43@gmail.com>
2024-01-03 10:40:00 +0530

    fix: move get post route at end

--- routes/post.js
+++ routes/post.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { post: { getFollowingUsersPost, getExplorePost, getPost } } = require('../controllers');
 
-router.get('/:id?', getPost);
 router.get('/following', getFollowingUsersPost);
 router.get('/explore', getExplorePost);
+router.get('/:id?', getPost);
 
 module.exports = router;
