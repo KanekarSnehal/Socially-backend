9d87afaa1dc96c4f4076e5fbc9d7a3e35ee4c9be
KanekarSnehal <kanekarsnehal43@gmail.com>
2024-01-26 14:39:40 +0530

    fix: add user to follow api route

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { user: { getUsersToFollow, findUserByUserName, updateUser } } = require('../controllers');
 
-// router.get('/follow', getUsersToFollow);
+router.get('/follow', getUsersToFollow);
 router.get('/:userName', findUserByUserName);
 router.post('/:userName', updateUser);
 module.exports = router;
