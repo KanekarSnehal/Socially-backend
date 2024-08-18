6c2f56d405520383aa501168d6e0b3d16b3075ea
KanekarSnehal <snehalkanekar43@gmail.com>
2024-08-19 00:40:05 +0530

    fix: change dialect to mysql

--- connections/mysql.js
+++ connections/mysql.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-    dialect: 'postgres',
+    dialect: 'mysql',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     username: process.env.DB_USER,
